232ad5bb681913a40a562793fcdba8b7c6b91834
benedikt <benedikt.oesing@tu-dortmund.de>
2021-01-14 06:59:16 +0100

    Changed the settings menu, hides SessionID, allows for fileselection to find the filter, shortens it.

--- src/ui/settings.js
+++ src/ui/settings.js
@@ -1,20 +1,20 @@
 // Vendor
-const {ipcRenderer} = require('electron');
+const { ipcRenderer } = require('electron');
 const settings = require('electron-settings');
 
 // Services
-const {fetchStashTabs} = require('../services/stash-tabs');
-const {fetchActiveLeagues} = require('../services/active-leagues');
+const { fetchStashTabs } = require('../services/stash-tabs');
+const { fetchActiveLeagues } = require('../services/active-leagues');
 
 const refreshStashTabs = async () => {
-  const {league, account, sessionId} = settings.get('user');
+  const { league, account, sessionId } = settings.get('user');
   if (!league || !account || !sessionId) return;
 
   const selectedStashIds = settings.get('user.stashIds') || [];
   const tabsElement = document.getElementById('tabs');
 
   try {
-    const stashTabs = await fetchStashTabs({league, account, sessionId});
+    const stashTabs = await fetchStashTabs({ league, account, sessionId });
 
     const tabsHtml = stashTabs.reduce((acc, stashTab) => {
       const isSelected = selectedStashIds.includes(stashTab.id);
@@ -78,7 +78,7 @@ fetchActiveLeagues().then((activeLeagues) => {
   leagueSelectElement.value = settings.get('user.league');
 });
 
-leagueSelectElement.onchange = async ({srcElement: {value}}) => {
+leagueSelectElement.onchange = async ({ srcElement: { value } }) => {
   settings.set('user.league', value);
   settings.set('user.stashIds', []);
   refreshStashTabs();
@@ -86,41 +86,49 @@ leagueSelectElement.onchange = async ({srcElement: {value}}) => {
 
 const accountInputElement = document.getElementById('account-input');
 accountInputElement.value = settings.get('user.account') || '';
-accountInputElement.oninput = async ({srcElement: {value}}) => {
+accountInputElement.oninput = async ({ srcElement: { value } }) => {
   settings.set('user.account', value);
   refreshStashTabs();
 };
 
 const sessionIdInputElement = document.getElementById('session-id-input');
 sessionIdInputElement.value = settings.get('user.sessionId') || '';
-sessionIdInputElement.oninput = async ({srcElement: {value}}) => {
+sessionIdInputElement.oninput = async ({ srcElement: { value } }) => {
   settings.set('user.sessionId', value);
   refreshStashTabs();
 };
 
 const RefreshTimeInputElement = document.getElementById('refresh-time-input');
 RefreshTimeInputElement.value = settings.get('overlay.refreshTime') || 30;
-RefreshTimeInputElement.oninput = async ({srcElement: {value}}) => {
+RefreshTimeInputElement.oninput = async ({ srcElement: { value } }) => {
   settings.set('overlay.refreshTime', value);
 };
 
 const OverlaySizeSelectElement = document.getElementById('overlay-size-select');
 OverlaySizeSelectElement.value = settings.get('overlay.size') || 1;
-OverlaySizeSelectElement.onchange = async ({srcElement: {value}}) => {
+OverlaySizeSelectElement.onchange = async ({ srcElement: { value } }) => {
   settings.set('overlay.size', value);
   ipcRenderer.send('overlay-size-changed');
 }
 
 const MaxSetsInputElement = document.getElementById('max-sets');
 MaxSetsInputElement.value = settings.get('user.maxSets') || 1;
-MaxSetsInputElement.oninput = async ({srcElement: {value}}) => {
+MaxSetsInputElement.oninput = async ({ srcElement: { value } }) => {
   settings.set('user.maxSets', value);
 };
 
 const mainFilterInputElement = document.getElementById('main-filter');
-mainFilterInputElement.value = settings.get('user.mainFilter') || '';
-mainFilterInputElement.oninput = async ({srcElement: {value}}) => {
+const mainFilterInputElementFileInput = document.getElementById('main-filter-fileInput');
+mainFilterInputElement.value = shortenURL(settings.get('user.mainFilter')) || '';
+mainFilterInputElementFileInput.oninput = async => {
+  const value = mainFilterInputElementFileInput.files[0].path;
   settings.set('user.mainFilter', value);
-};
+  mainFilterInputElement.value = shortenURL(value);
+}
+
+function shortenURL(url) {
+  const parts = url.split("\\");
+  return parts[parts.length - 1];
+}
 
 refreshStashTabs();
